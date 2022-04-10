39e3eee2c8d3aca7e7099185439a54799126735f
shhady <shhadyse@gmail.com>
2022-04-10 21:13:42 +0300

    languages

---
+++ app.js
@@ -0,0 +1,15 @@
+let language = "english";
+
+switch (language){
+    case 'mandarin': console.log("most speaking language");
+    break;
+    case 'spanish':console.log("2nd place in number of native speakers");
+    break;
+    case 'english': console.log("3rd place");
+    break;
+    case 'hindi': console.log("number 4");
+    break;
+    case 'arabic':console.log("5th most spoken language");
+    break;
+    default: console.log('Not in the top 5');
+}
